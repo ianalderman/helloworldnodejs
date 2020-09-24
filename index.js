3693a8dd66d8dad088a7123271b005eaec91845b
Ian Alderman <ialderman@hotmail.co.uk>
2020-09-24 17:14:07 +0100

    Revert "Trying simpler version else will revert"
    This reverts commit ec677f66a175f561b182c4d9d8a6c36b3cfa0b20.

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-      var app = express()
-      var os = require('os');
-      app.get('/', function (req, res) {
-        res.send('Hello World from ' + process.env.REGION_NAME())
-      })
-      app.listen(process.env.PORT, function () {
-        console.log('Hello world app listening')
-      })
